6956f50a671174a3c41c73c764e916148f30c63f
Alex Balter <albalter@gmail.com>
2025-08-08 18:23:25 +0300

    Solves exercise 9

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,23 @@
-const sumAll = function() {
+const sumAll = function(k, n) {
+    if (typeof k !== "number" || typeof n !== "number"){
+        return "ERROR"
+    }
+
+    if (!Number.isInteger(k) || !Number.isInteger(n)) {
+        return "ERROR";
+    }
+
+    if (k < 0 || n < 0) {
+        return "ERROR"
+    }
+
+    if (k > n)
+    {
+        let p = k;
+        k = n;
+        n = p;
+    }
+    return (n*n-k*k+n+k)/2
 
 };
 
