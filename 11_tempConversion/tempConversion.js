504c21c78ead7db5b78d32401cdf00f3a6b69a16
Alex Balter <albalter@gmail.com>
2025-08-11 12:32:20 +0300

    Solutions to a few of the exercises

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  //farenheit to celsius
+  temp = (temp -32)*5/9
+  return Number(Number.parseFloat(temp).toFixed(1))
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  //celsius to farenheit
+  temp = temp * 9/5 + 32
+  return Number(Number.parseFloat(temp).toFixed(1))
 };
 
 // Do not edit below this line
--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,25 @@
-const palindromes = function () {
-
+/* Pseudocode 
+    1.strip all punctuation signs and spaces from 
+    the string, since they don't count as a symbol
+    for the purpose of testing our function.
+    2. convert the string to lowercase
+    3. iterate over the string, first comparing 
+    the first and last character, then 2nd and 
+    last -1, etc. the condition of termination is
+    when the indexes being compared meet in the middle
+    Initially, assume the string to be a palindrome, 
+    but if at any stage the comparison fails, break
+    out fo the cycle and return from function with false.
+    Otherwise, return true.
+    end.
+*/
+const palindromes = function (str) {
+    str.replace(/[^a-zA-Z0-9\s]/g, '').toLowerCase()
+    for (let i=0, j=str.length;i<=j;i++,j--)
+        if (str[i] !== str[j]) {
+            return false;
+        }
+    return true;
 };
 
 // Do not edit below this line
