f5b23d74b567727ac7d742ba86295a888df93bf5
Alex Balter <albalter@gmail.com>
2025-08-11 14:36:48 +0300

    The final exercise

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,33 @@
-const findTheOldest = function() {
+/*Pesudocode 
+    structure of the people object is the following
+    {
+        name :
+        yearOfBirth:
+        yearOfDeath:
+    }
+    we have to implement function that finds the maximum
+    out of every record, comparing them with function age()
+    that gets the difference between year of birth and death    
+*/
 
+function age (person) {
+    if (person.hasOwnProperty('yearOfBirth')) {
+        birth = person.yearOfBirth
+    }
+    if (person.hasOwnProperty('yearOfDeath')) {
+        current = person.yearOfDeath
+    } else {
+        current = new Date().getFullYear();
+    }
+    
+    return current - birth
+}
+
+const findTheOldest = function(people) {
+    people.sort((a, b) => (age(a)>age(b)?-1:1))
+    console.table(people);
+    console.log(people[0].name);
+    return people[0];
 };
 
 // Do not edit below this line
